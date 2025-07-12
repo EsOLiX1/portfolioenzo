c198ae010d7e4eea60837e5468753320ef8efd3c
Enzo <azalbertenzo@gmail.com>
2025-07-12 20:57:37 +0200

    mise à jour page stage 1 terminé !!!!!, mise à jour du site

--- stage1.js
+++ stage1.js
@@ -11,7 +11,7 @@ const galleriesData = {
 
   ],
   TableauSMS: [
-    { src: 'imgstage1/SMS Logiciel/FEN_SMS.png', alt: 'FEN_SMS', caption: 'Écran de la fenêtre du tableau des SMS.' },
+    { src: 'imgstage1/SMS Logiciel/FEN_SMS.png', alt: 'FEN_SMS', caption: 'Écran du tableau des SMS.' },
     { src: 'imgstage1/SMS Logiciel/SAI_RECHDEB_FIN.png', alt: 'SAI_RECHDEB_FIN', caption: 'Voici le code du filtre "période du" "au" il contient juste la fonction RempliTableSQL2 que nous verrons un peu plus tard.' },
     { src: 'imgstage1/SMS Logiciel/COMBO_PORT.png', alt: 'COMBO_PORT', caption: 'Nous avons là le code du menu déroulant permettant de sélectionner un nom de port pour filtrer.' },
     { src: 'imgstage1/SMS Logiciel/BTN_Ajouter.png', alt: 'BTN_Ajouter', caption: 'Le code du bouton ajouter permet juste d’ouvrir et de passer en paramètre le mot "Création" à une fenêtre que nous verrons par la suite qui est fenêtre "fiche des SMS".' },
@@ -25,7 +25,7 @@ const galleriesData = {
   ],
 
   TableauPorts: [
-    { src: 'imgstage1/Ports Logiciel/FEN_Ports.png', alt: 'FEN_Ports', caption: 'Écran de la fenêtre du tableau des Ports.' },
+    { src: 'imgstage1/Ports Logiciel/FEN_Ports.png', alt: 'FEN_Ports', caption: 'Écran du tableau des Ports.' },
     { src: 'imgstage1/Ports Logiciel/BTN_Ajouter.png', alt: 'BTN_Ajouter', caption: 'Le code du bouton ajouter permet juste d’ouvrir et de passer en paramètre le mot "Création" à une fenêtre que nous verrons par la suite qui est fenêtre "fiche des Ports".' },
     { src: 'imgstage1/Ports Logiciel/BTN_Modifier.png', alt: 'BTN_Modifier', caption: 'Le code du bouton du bouton modifier est assez similaire à celui d’ajouter sauf qui oblige l’utilisateur à sélectionner une seule ligne et que le paramètre est "Modif".' },
     { src: 'imgstage1/Ports Logiciel/BTN_Supprimer.png', alt: 'BTN_Supprimer', caption: 'Le code du bouton supprimer est assez simple il exige une seule ligne sélectionner puis il utilise la fonction Hlitrecherche et il Hsupprime.' },
@@ -34,7 +34,7 @@ const galleriesData = {
   ],
   
   TableauComptabilité: [
-    { src: 'imgstage1/Comptabilité Logiciel/FEN_COMPTA.png', alt: 'FEN_COMPTA', caption: 'Écran de la fenêtre du tableau de Comptabilité.' },
+    { src: 'imgstage1/Comptabilité Logiciel/FEN_COMPTA.png', alt: 'FEN_COMPTA', caption: 'Écran du tableau de Comptabilité.' },
     { src: 'imgstage1/Comptabilité Logiciel/SAI_RECHDEB_FIN.png', alt: 'SAI_RECHDEB_FIN', caption: 'Voici le code du filtre "période du" "au" il contient juste la fonction RempliTableSQL2 que nous verrons un peu plus tard.' },
     { src: 'imgstage1/Comptabilité Logiciel/COMBO_PORT.png', alt: 'COMBO_PORT', caption: 'Nous avons là le code du menu déroulant permettant de sélectionner un nom de port pour filtrer.' },
     { src: 'imgstage1/Comptabilité Logiciel/BTN_Ajouter.png', alt: 'BTN_Ajouter', caption: 'Le code du bouton ajouter permet juste d’ouvrir et de passer en paramètre le mot "Création" à une fenêtre que nous verrons par la suite qui est fenêtre "fiche des Comptabilité".' },
@@ -47,13 +47,111 @@ const galleriesData = {
   ],
 
   TableauSoldes: [
-    { src: 'imgstage1/Solde Logiciel/FEN_Solde_des_Ports.png', alt: 'FEN_Solde_des_Ports', caption: 'Écran de la fenêtre du tableau des Soldes.' },
+    { src: 'imgstage1/Solde Logiciel/FEN_Solde_des_Ports.png', alt: 'FEN_Solde_des_Ports', caption: 'Écran du tableau des Soldes.' },
     { src: 'imgstage1/Solde Logiciel/BTN_COMPTA.png', alt: 'BTN_COMPTA', caption: 'Le code du bouton "Comptabilité" permet d’ouvrir la fenêtre tableau de Comptabilité avec en paramètre le code du port qui permet d’afficher uniquement le port sélectionné' },
-    { src: 'imgstage1/Solde Logiciel/BTN_Mail(1).png', alt: 'BTN_Mail(1)', caption: 'Le code du bouton du bouton modifier est assez similaire à celui d’ajouter sauf qui oblige l’utilisateur à sélectionner une seule ligne et que le paramètre est "Modif".' },
-    { src: 'imgstage1/Solde Logiciel/BTN_Mail(2).png', alt: 'BTN_Mail(2)', caption: 'Le code du bouton supprimer est assez simple il exige une seule ligne sélectionner puis il utilise la fonction Hlitrecherche et il Hsupprime.' },
-    { src: 'imgstage1/Solde Logiciel/Déclaration(1).png', alt: 'Déclaration(1)', caption: 'Voici les déclaration de la fenêtre tableau des Ports.' },
-    { src: 'imgstage1/Solde Logiciel/Déclaration(2).png', alt: 'Déclaration(2)', caption: 'Voici les déclaration de la fenêtre tableau des Ports.' },
+    { src: 'imgstage1/Solde Logiciel/BTN_Mail(1).png', alt: 'BTN_Mail(1)', caption: 'Le code du bouton envoyer un mail récupère les emails les ports et informe l’utilisateur si le port ne possède pas de mail.' },
+    { src: 'imgstage1/Solde Logiciel/BTN_Mail(2).png', alt: 'BTN_Mail(2)', caption: 'Mais il informe aussi l’utilisateur si les soldes ne sont pas les même' },
+    { src: 'imgstage1/Solde Logiciel/Déclaration(1).png', alt: 'Déclaration(1)', caption: 'Voici les déclaration de la fenêtre tableau des Soldes.' },
+    { src: 'imgstage1/Solde Logiciel/Déclaration(2).png', alt: 'Déclaration(2)', caption: 'Voici les déclaration de la fenêtre tableau des Soldes.' },
 
+  ],
+
+  ModèleMail: [
+    { src: 'imgstage1/Modèle Email Logiciel/FEN_MODELEEMAIL.png', alt: 'FEN_MODELEEMAIL', caption: 'Écran du tableau des Modèles de Mail.' },
+    { src: 'imgstage1/Modèle Email Logiciel/BTN_Ajouter.png', alt: 'BTN_Ajouter', caption: 'Le code du bouton ajouter permet juste d’ouvrir et de passer en paramètre le mot "Création" à une fenêtre que nous verrons par la suite qui est fenêtre "fiche des Modèles de Mail".' },
+    { src: 'imgstage1/Modèle Email Logiciel/BTN_Modifier.png', alt: 'BTN_Modifier', caption: 'Le code du bouton du bouton modifier est assez similaire à celui d’ajouter sauf que le paramètre est "Modif".' },
+    { src: 'imgstage1/Modèle Email Logiciel/BTN_Supprimer.png', alt: 'BTN_Supprimer', caption: 'Le code du bouton supprimer est assez simple il utilise la fonction Hlitrecherche et il Hsupprime.' },
+    { src: 'imgstage1/Modèle Email Logiciel/Déclaration.png', alt: 'Déclaration', caption: 'Voici les déclaration de la fenêtre tableau des Modèles de Mail.' },
+
+  ],
+
+  Jauge: [
+    { src: 'imgstage1/Jauge Logiciel/FEN_Jauge.png', alt: 'FEN_Jauge', caption: 'Voici la jauge de chargement lors de l’importation des données.' },
+    { src: 'imgstage1/Jauge Logiciel/Déclaration.png', alt: 'Déclaration', caption: 'Le code de la Jauge est la suite du code de la fonction importation.' },
+  
+  ],
+
+  FicheSMS: [
+    { src: 'imgstage1/Fiche LISTSMS Logiciel/FEN_Fiche_LISTSMS.png', alt: 'FEN_Fiche_LISTSMS', caption: 'Écran de la fiche SMS.' },
+    { src: 'imgstage1/Fiche LISTSMS Logiciel/BTN_Valider.png', alt: 'BTN_Valider', caption: 'Le code du bouton valider est juste la pour récupérer le mot "Création" ou "Modif" et donc soit ajouter soit modifier.' },
+    { src: 'imgstage1/Fiche LISTSMS Logiciel/Déclaration.png', alt: 'Déclaration', caption: 'Le code de la fiche SMS récupère le mot "Création" ou "Modif" pour récupérer ou non les données suivant le mode.' },
+  
+  ],
+
+  FichePorts: [
+    { src: 'imgstage1/Fiche Port Logiciel/FEN_Fiche_PORT.png', alt: 'FEN_Fiche_PORT', caption: 'Écran de la fiche Port.' },
+    { src: 'imgstage1/Fiche Port Logiciel/BTN_Valider.png', alt: 'BTN_Valider', caption: 'Le code du bouton valider est juste la pour récupérer le mot "Création" ou "Modif" et donc soit ajouter soit modifier.' },
+    { src: 'imgstage1/Fiche Port Logiciel/Déclaration.png', alt: 'Déclaration', caption: 'Le code de la fiche SMS récupère le mot "Création" ou "Modif" pour récupérer ou non les données suivant le mode.' },
+  
+  ],
+
+  FicheComptabilité: [
+    { src: 'imgstage1/Fiche Compta Logiciel/FEN_Fiche_COMPTA.png', alt: 'FEN_Fiche_COMPTA', caption: 'Écran de la fiche Comptabilité.' },
+    { src: 'imgstage1/Fiche Compta Logiciel/BTN_Valider.png', alt: 'BTN_Valider', caption: 'Le code du bouton valider est juste la pour récupérer le mot "Création" ou "Modif" et donc soit ajouter soit modifier.' },
+    { src: 'imgstage1/Fiche Compta Logiciel/Déclaration.png', alt: 'Déclaration', caption: 'Le code de la fiche SMS récupère le mot "Création" ou "Modif" pour récupérer ou non les données suivant le mode.' },
+  
+  ],
+
+  FicheModeleMail: [
+    { src: 'imgstage1/Fiche modèle de Mail/FEN_Fiche_MODELEEMAIL.png', alt: 'FEN_Fiche_MODELEEMAIL', caption: 'Écran de la fiche Comptabilité.' },
+    { src: 'imgstage1/Fiche modèle de Mail/BTN_Valider.png', alt: 'BTN_Valider', caption: 'Le code du bouton valider est juste la pour récupérer le mot "Création" ou "Modif" et donc soit ajouter soit modifier.' },
+    { src: 'imgstage1/Fiche modèle de Mail/Déclaration.png', alt: 'Déclaration', caption: 'Le code de la fiche SMS récupère le mot "Création" ou "Modif" pour récupérer ou non les données suivant le mode.' },
+  
+  ],
+
+  FicheNom: [
+    { src: 'imgstage1/Fiche Ponom Logiciel/FEN_Fiche_PONOM.png', alt: 'FEN_Fiche_PONOM', caption: 'Écran de la fiche Nom.' },
+    { src: 'imgstage1/Fiche Ponom Logiciel/BTN_Valider.png', alt: 'BTN_Valider', caption: 'Le code du bouton "Valider" cherche dans la table "PORTIGNORE" pour voir si le port en question n’a pas déjà été ignoré puis il cherche l’ID de l’enregistrement pour ajouter le nom à la bonne ligne de la table.' },
+    { src: 'imgstage1/Fiche Ponom Logiciel/BTN_Ignorer.png', alt: 'BTN_Ignorer', caption: 'Le code du bouton "ignorer" cherche dans la table "PORTIGNORE" pour voir si le port en question n’a pas déjà été ignoré puis il ajoute le port à la table "PORTIGNORE".' },
+    { src: 'imgstage1/Fiche Ponom Logiciel/Déclaration.png', alt: 'Déclaration', caption: 'Dans la déclaration de la fenêtre j’initialise juste le code du port.' },
+
+  ],
+
+  EnvoiMail: [
+    { src: 'imgstage1/Envoi de Mail Logiciel/FEN_Envoi_de_Mail.png', alt: 'FEN_Envoi_de_Mail', caption: 'Écran de la fiche Comptabilité.' },
+    { src: 'imgstage1/Envoi de Mail Logiciel/COMBO_MODELEEMAIL.png', alt: 'COMBO_MODELEEMAIL', caption: 'Le code de la combo permet juste d’avoir le nom associé à l’objet et au texte du mail un fois sélectionné celà rempli les autres cases.' },
+    { src: 'imgstage1/Envoi de Mail Logiciel/BTN_Envoyer(1).png', alt: 'BTN_Envoyer(1)', caption: 'Le code du bouton "Envoyer" permet de se connecter au serveur SMTP de l’entreprise puis il récupère la date du dernier achat du port sélectionné ainsi que le mail du port.' },
+    { src: 'imgstage1/Envoi de Mail Logiciel/BTN_Envoyer(2).png', alt: 'BTN_Envoyer(2)', caption: 'Toujours le bouton "Envoyer" transforme les mots avec un "#" en valeurs voulu dans le corps du mail et un fichier est généré avec les différents SMS envoyé par le port puis se dernier est ajouté en pièce jointe.' },
+    { src: 'imgstage1/Envoi de Mail Logiciel/BTN_Envoyer(3).png', alt: 'BTN_Envoyer(3)', caption: 'Pour finir, un message est envoyé à l’utilisateur sur le statut de l’envoi du mail.' },
+    { src: 'imgstage1/Envoi de Mail Logiciel/Déclaration(1).png', alt: 'Déclaration(1)', caption: 'La procédure ExporteHistoirique s’occupe de générer le ficher des SMS du port pour qu’ils aient un suivi des différents SMS envoyés.' },
+    { src: 'imgstage1/Envoi de Mail Logiciel/Déclaration(2).png', alt: 'Déclaration(2)', caption: 'Puis ce dernier est ajouté en pièce jointe du Mail.' },
+
+  ],
+
+  Analyse: [
+    { src: 'imgstage1/Analyse Logiciel/Analyse.png', alt: 'Analyse', caption: 'Écran de l’analyse.' },
+    { src: 'imgstage1/Analyse Logiciel/Analyse LISTSMS.png', alt: 'Analyse LISTSMS', caption: 'Analyse LISTSMS' },
+    { src: 'imgstage1/Analyse Logiciel/Analyse COMPTA.png', alt: 'Analyse COMPTA', caption: 'Analyse COMPTA' },
+    { src: 'imgstage1/Analyse Logiciel/Analyse PORT.png', alt: 'Analyse PORT', caption: 'Analyse PORT' },
+    { src: 'imgstage1/Analyse Logiciel/Analyse PORTIGNORE.png', alt: 'Analyse PORTIGNORE', caption: 'Analyse PORTIGNORE' },
+    { src: 'imgstage1/Analyse Logiciel/Analyse MODELEEMAIL.png', alt: 'Analyse MODELEEMAIL', caption: 'Analyse MODELEEMAIL' },
+
+  ],
+
+  FonctionnementWindev: [
+    { src: 'imgstage1/Fonctionnement Windev/Bandeau Supérieur Onglet Projet.png', alt: 'Bandeau Supérieur Onglet Projet', caption: 'Bandeau Supérieur Onglet Projet' },
+    { src: 'imgstage1/Fonctionnement Windev/Bandeau Supérieur Onglet GDS.png', alt: 'Bandeau Supérieur Onglet GDS', caption: 'Bandeau Supérieur Onglet GDS' },
+    { src: 'imgstage1/Fonctionnement Windev/Bandeau Supérieur Onglet Création.png', alt: 'Bandeau Supérieur Onglet Création', caption: 'Bandeau Supérieur Onglet Création' },
+    { src: 'imgstage1/Fonctionnement Windev/Bandeau Supérieur Onglet Outils.png', alt: 'Bandeau Supérieur Onglet Outils', caption: 'Bandeau Supérieur Onglet Outils' },
+  
+  ],
+
+  Global: [
+    { src: 'imgstage1/Global  Principal/Ecran principal Windev.png', alt: 'Ecran principal Windev', caption: 'Écran principal Windev' },
+    { src: 'imgstage1/Global  Principal/Varriables Globales.png', alt: 'Varriables Globales', caption: 'Varriables Globales : il s’agit des variables qui sont gloables au projet on peut les utiliser n’importe où.' },
+  
+  ],
+
+  ProceduresGlobales: [
+    { src: 'imgstage1/Procédures Globales Logiciel/Procédure Importation.png', alt: 'Procédure Importation', caption: 'La fonction "Importation" permet de récupérer les données d’un fichier excel pour ajouter les données aux différentes analyses.' },
+    { src: 'imgstage1/Procédures Globales Logiciel/Procédure ActualiseListeDesPorts.png', alt: 'Procédure ActualiseListeDesPorts', caption: 'La fonction "Actualise la liste des ports" permet d’associer les codes et les ID aux noms des ports.' },
+    { src: 'imgstage1/Procédures Globales Logiciel/Procédure ActualiseLaCompta.png', alt: 'Procédure ActualiseLaCompta', caption: 'La fonction "Actualise la Compta" permet de remplir l’analyse COMPTA à partir des données importées.' },
+    { src: 'imgstage1/Procédures Globales Logiciel/Procédure CnxAlizSMS.png', alt: 'Procédure CnxAlizSMS', caption: 'La fonction "CnxAlizSMS" permet de se connecter au serveur pour y ajouter les données ou les utiliser.' },
+    { src: 'imgstage1/Procédures Globales Logiciel/Procédure RecupAMDID(1).png', alt: 'Procédure RecupAMDID(1)', caption: 'La fonction "RecupAMDID" permet de se connecter au serveur de données de l’entreprise pour y récupérer des données notament les ID des ports ou encore les Mails des dirigéants des différents ports.' },
+    { src: 'imgstage1/Procédures Globales Logiciel/Procédure RecupAMDID(2).png', alt: 'Procédure RecupAMDID(2)', caption: 'La fonction "RecupAMDID".' },
+    { src: 'imgstage1/Procédures Globales Logiciel/Procédure RecupAMDID(3).png', alt: 'Procédure RecupAMDID(3)', caption: 'La fonction "RecupAMDID".' },
+    { src: 'imgstage1/Procédures Globales Logiciel/Procédure RecupAMDID(4).png', alt: 'Procédure RecupAMDID(4)', caption: 'La fonction "RecupAMDID".' },
+  
   ],
   // Ajoute d’autres galeries si besoin
 };
